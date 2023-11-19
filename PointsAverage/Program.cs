0c83ae5c77626093eab86dc9ba6c6d26b1718144
Артем Ващенко <ipz231_vai@student.ztu.edu.ua>
2023-11-19 23:18:12 +0200

    Task 2 updated and finished

--- PointsAverage/Program.cs
+++ PointsAverage/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointsAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            int sum = 0;
+            for (int i = 0; i < 5; i++) sum += inputProcedure("> ");
+            if (sum / 5 >= 50) Console.WriteLine("> Студента допущено до екзамену.");
+            else Console.WriteLine("> Студента не допущено до екзамену.");
+
+
+        }
+
+        private static int inputProcedure(string ctext)
+        {
+            while (true)
+            {
+                Console.Write(ctext);
+                if (int.TryParse(Console.ReadLine(), out int outval) && outval <= 100 && outval >= 0) return outval;
+                else Console.WriteLine("Wrong input.");
+            }
+        }
+    }
+}
